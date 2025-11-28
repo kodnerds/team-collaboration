9e10bb42fbedc14d3c56f274a0e3b7baeb6a7e38
fmdavids <fmproade@gmail.com>
2025-11-28 14:32:28 +0100

    feat: implement forgot-password

--- apps/api/src/routes/users.routes.ts
+++ apps/api/src/routes/users.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
-import { login, signup } from '../controller';
-import { loginValidator, signupValidator, validate } from '../middleware';
+import { forgotPassword, login, signup } from '../controller';
+import { forgotPasswordValidator, loginValidator, signupValidator, validate } from '../middleware';
 
 const router = Router();
 
 router.post('/login', loginValidator, validate, login);
 router.post('/signup', signupValidator, validate, signup);
+router.post(
+  '/forgot-password',
+  forgotPasswordValidator,
+  forgotPassword
+);
 
 export default router;
