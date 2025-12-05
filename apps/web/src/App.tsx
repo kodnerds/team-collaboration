b66bb954c4820d575124d4b4f7bbcad7c6541e82
sophiee <xophieadrian@gmail.com>
2025-12-05 14:50:57 +0100

    feat: sign up screen

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Login from './components/Login';
+import SignupPage from './components/SignUp/SignupPage';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Navigate to="/login" />} />
     <Route path="/login" element={<Login />} />
     <Route path="/dashboard" element={<div>Dashboard Placeholder</div>} />
+    <Route path="/signup" element={<SignupPage />} />
   </Routes>
 );
 
