6df4f2139c77d6175454b20602d29f0678b2c317
sophiee <xophieadrian@gmail.com>
2025-12-09 22:56:03 +0100

    Feat: merged main to edit-project branch
    Merged create-project and project details page into my branch

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 
 import CreateProject from './components/Create-project';
 import { KanbanBoard } from './components/KanbanBoard';
+import EditProject from './components/EditProject';
 import Login from './components/Login';
 import ProjectsList from './components/ProjectsList';
 import SignupPage from './components/SignUp/SignupPage';
@@ -14,6 +15,7 @@ const App = () => (
     <Route path="/create-project" element={<CreateProject />} />
     <Route path="/projects" element={<ProjectsList />} />
     <Route path="/projects/:id" element={<KanbanBoard />} />
+    <Route path="edit-project/:id" element={<EditProject />} />
     <Route path="/signup" element={<SignupPage />} />
   </Routes>
 );
