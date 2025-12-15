3a89fa0aab14d76319cf771fab04d6dbd338ea3a
Soph-iee <xophieadrian@gmail.com>
2025-12-15 23:35:49 +0100

    feat: update EditProject component with form validation and navigation

--- apps/web/src/components/EditProject.tsx
+++ apps/web/src/components/EditProject.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import InputField from './SignUp/inputField';
 
@@ -16,17 +16,72 @@ const EditProject = () => {
       );
   }, []);
 
+  const navigate = useNavigate();
   const [projectDetails, setProjectDetails] = useState({
     name: '',
     description: ''
   });
-
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [successMessage, setSuccessMessage] = useState('');
+  const [error, setError] = useState('');
+  const [_, setApiError] = useState('');
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setProjectDetails((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
-  //   const handleSubmit=(){
-  //   }
+  const clientSideValidation = () => {
+    let isFormValid = true;
+    // checking name field
+    if (!projectDetails.name.trim()) {
+      isFormValid = false;
+      setError('Please input a valid name');
+    }
+    // checking for name length
+    if (projectDetails.name.trim().length < 3) {
+      isFormValid = false;
+      setError('Project name must be at least 3 characters');
+    }
+
+    return isFormValid;
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const base = import.meta.env.VITE_API_URL;
+    setError('');
+    setSuccessMessage('');
+    if (!clientSideValidation()) {
+      return;
+    }
+    setIsSubmitting(true);
+    try {
+      const response = await fetch(`${base}/auth/project/:id`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(projectDetails)
+      });
+      const data = await response.json();
+      if (response.status === 201) {
+        const { id, name } = data.data;
+        localStorage.setItem('userSession', JSON.stringify({ id, name }));
+        setProjectDetails({ ...projectDetails, name: '', description: '' });
+        setSuccessMessage('Project created successfully!');
+
+        setTimeout(() => {
+          navigate('/projects');
+        }, 500);
+      } else if (response.status === 400 || response.status === 409) {
+        setApiError(data.message || `API Error: Status ${response.status}`);
+      } else {
+        setApiError('An unexpected server error occurred. Please try again.');
+      }
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
     <main className="min-h-screen bg-linear-to-br from-blue-50 via-purple-50 to-pink-50 flex items-center justify-center p-4 relative overflow-hidden">
       <div className="bg-white rounded-lg shadow-xl w-full px-10 py-5  max-w-[450px] relative z-10">
@@ -47,7 +102,7 @@ const EditProject = () => {
           </Link>
           <h2 className="text-2xl font-semibold text-gray-700">Edit Project</h2>
         </div>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <InputField
             error=""
             htmlFor="project-name"
@@ -59,6 +114,7 @@ const EditProject = () => {
             name="name"
           />
           <div>
+            <p>{error ?? error} </p>
             <label
               htmlFor="description"
               className="input-label block text-sm font-semibold text-gray-700  mb-1"
@@ -76,25 +132,24 @@ const EditProject = () => {
               className="w-full px-3 py-2.5 mb-1 border-2 border-gray-300 rounded focus:border-blue-500 focus:outline-none bg-white text-black"
             ></textarea>
           </div>
-          {/* <InputField
-            error="error"
-            htmlFor="project-description"
 
-            text="Description"
-            type="text"
-
-            onChange={handleChange}
-
-          /> */}
-          <div>
+          <div className="flex justify-end space-x-4">
+            <button
+              type="button"
+              className="w-20 py-3 bg-white border  border-gray-300 text-blue-600 rounded-lg hover:bg-blue-600 transition font-medium   disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center gap-2"
+              onClick={() => {
+                navigate('/project');
+              }}
+            >
+              Cancel
+            </button>
             <button
               type="submit"
-              className="w-20 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition font-medium  mx-auto disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center gap-2"
-            //   disabled={isSubmitting || !!successMessage}
+              className="w-20 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition font-medium   disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center gap-2"
+              disabled={isSubmitting || !!successMessage}
             >
               Save
             </button>
-            <button></button>
           </div>
         </form>
       </div>
